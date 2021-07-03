f7821877c8ac47c74325f4915e1d946c2ffda878
ramoshe <rsherard@mac.com>
2021-07-03 02:02:07 -0400

    createTokens method added

--- js/Player.js
+++ js/Player.js
@@ -4,6 +4,20 @@ class Player {
         this.id = id;
         this.color = color;
         this.active = active;
-        this.tokens = [];
+        this.tokens = this.createTokens(21);
+    }
+
+    /**
+     * Creates token objects for player
+     * @param     {number}    num - Number of token objects to be created
+     * @returns   {Array}     An array of the newly created token objects
+     */
+    createTokens(num) {
+        const tokens = [];
+        for (let i=0; i<num; i++) {
+            let token = new Token(i, this);
+            this.tokens.push(token);
+        }
+        return tokens;
     }
 }
